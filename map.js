07f23cd2d69872d03fdef950defed3e95bd26155
garshasbi-f <ng.garshasbi@gmail.com>
2023-01-15 02:54:55 +0330

    add leaflet & tile layer

---
+++ map.js
@@ -0,0 +1,67 @@
+// Map initialization
+var map = L.map("map").setView([34.4279, 53.688], 7);
+
+/*==============================================
+                TILE LAYER and WMS
+================================================*/
+
+//OSM LAYER
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
+osm.addTo(map);
+
+//WATER COLOR
+var watercolor = L.tileLayer(
+  "https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}",
+  {
+    attribution:
+      'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: "abcd",
+    minZoom: 1,
+    maxZoom: 16,
+    ext: "jpg",
+  }
+);
+
+//DARK MAP
+var dark = L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
+    maxZoom: 19,
+  }
+);
+
+//GOOGLE STREET
+googleStreets = L.tileLayer(
+  "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
+
+//GOOGLE SATELLITE
+googleSat = L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
+  maxZoom: 20,
+  subdomains: ["mt0", "mt1", "mt2", "mt3"],
+});
+
+/*==============================================
+            LAYER CONTROL
+================================================*/
+
+var baseMaps = {
+  "OSM": osm,
+  "Water color map": watercolor,
+  "Dark": dark,
+  "Google Street": googleStreets,
+  "Google Satellite": googleSat,
+};
+
+
+L.control.layers(baseMaps).addTo(map);
