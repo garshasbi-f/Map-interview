67414be2becd89ca9752759b3656bcf2a0e8a0db
garshasbi-f <ng.garshasbi@gmail.com>
2023-01-15 03:25:35 +0330

    add new markers

--- map.js
+++ map.js
@@ -37,16 +37,27 @@ makeTileLayer(mapList[0]).addTo(map);
 const citieMarkersInfo = [
   [35.7219, 51.3347, "img/red_marker.png", "img/Tehran.png", "Tehran"],
   [32.6539, 51.666, "img/red_marker.png", "img/Esfehan.png", "Esfehan"],
+  [34.0873, 49.7022, "img/red_marker.png", "img/Arak.png", "Arak"],
+  [34.6416, 50.8746, "img/red_marker.png", "img/Qom.png", "Qom"],
+  [31.8974, 54.3569, "img/red_marker.png", "img/Yazd.png", "Yazd"],
+  [36.2972, 59.6067, "img/red_marker.png", "img/Mashhad.png", "Mashhad"],
+  [38.2432, 48.2976, "img/red_marker.png", "img/Ardebil.png", "Ardebil"],
+  [37.2713, 49.5921, "img/red_marker.png", "img/Rasht.png", "Rasht"],
 ];
 
 const lakeMarkersInfo = [
   [34.497506, 51.816561, "img/lake.png", "Namak Lake"],
   [37.352424, 52.04343, "img/lake.png", "Caspian Sea"],
+  [35.89712, 54.664699, "img/lake.png", "HajAligholiSaltLake"],
+  [34.992335, 50.93811, "img/lake.png", "HozeSoltanSaltLake"],
+  [37.51063, 45.410502, "img/lake.png", "LakeUrmia"],
 ];
 
 const jungleMarkersInfo = [
   [36.782996, 54.462093, "img/jungle.png", "NaharkhoranPark"],
   [32.837736, 51.837931, "img/jungle.png", "NazhvanPark"],
+  [38.907556, 46.865183, "img/jungle.png", "ArasbaranProtectedArea"],
+  [36.753034, 55.031641, "img/jungle.png", "AbrForest"],
 ];
 
 function makeLayerGroup(pointsInfo) {
